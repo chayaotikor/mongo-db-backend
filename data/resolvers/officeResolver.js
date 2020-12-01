1e042ca8ece0f4ccb115ddfddfaa93eeb8e49bfa
Chaya Otikor <c.otikor@gmail.com>
2020-12-01 09:18:51 -0700

    office resolvers

--- data/resolvers/officeResolver.js
+++ data/resolvers/officeResolver.js
@@ -1,20 +1,84 @@
 const Office = require("../models/officeModel")
-const Candidate = require("../models/candidateModel")
-const Duty = require("../models/officeModel")
+const errorHandler = require("../../config/errorHandler")
 
 module.exports = {
-    addOffice: async ({title, duties, candidates}) => {
-        const officeDetails = new Office({
-            title: title,
-            duties: duties,
-            candidates: candidates
-        })
-        try {
-            const newOffice = await officeDetails.save();
+  getAllOffices: async () => {
+    try {
+      const offices = await Office.find();
 
-            return {...newOffice._doc}
-        } catch (error) {
-            console.log(error)
-        }
+      return offices.map((office) => {
+        return { ...office._doc };
+      });
+    } catch (err) {
+      errorHandler(err);
     }
-}
+  },
+  getOffice: async ({ id }) => {
+    try {
+      const office = await Office.findOne({ _id: id });
+      if (!office) {
+        errorHandler(responseStatus.notFound);
+      }
+      return { ...office._doc };
+    } catch (err) {
+      errorHandler(err);
+    }
+  },
+
+  addOffice: async ({ content }) => {
+    const officeContent = new Office({
+      title: content.title,
+      duties: content.duties,
+      candidates: [],
+    });
+    try {
+      if (
+        officeContent.title === "undefined" ||
+        officeContent.duties === "undefined"
+      ) {
+        errorHandler(responseStatus.badRequest);
+      }
+      const newOffice = await officeContent.save();
+
+      return { ...newOffice._doc };
+    } catch (err) {
+      errorHandler(err);
+    }
+  },
+
+  updateOffice: async ({ content, id }) => {
+    try {
+      const office = await Office.findOne({ _id: id });
+      if (!office) {
+        errorHandler(responseStatus.notFound);
+      } else {
+        if (content.title) {
+          office.title = content.title;
+        }
+        if (content.duties) {
+          office.duties = content.duties;
+        }
+        if (content.candidates) {
+          office.candidates = content.candidates;
+        }
+        await office.save();
+        return { ...office._doc };
+      }
+    } catch (err) {
+      errorHandler(err);
+    }
+  },
+
+  deleteOffice: async ({ officeId }) => {
+    try {
+      const office = await Office.findOne({ _id: officeId });
+      if (!office) {
+        errorHandler(responseStatus.notFound);
+      } else {
+        office.remove();
+      }
+    } catch (err) {
+      errorHandler(err);
+    }
+  },
+};
