dfacd869bf66a4ce72c1b6e2fabed72e7d14c236
Chaya Otikor <c.otikor@gmail.com>
2020-12-01 08:33:05 -0700

    root resolver and error handler config

---
+++ config/errorHandler.js
@@ -0,0 +1,22 @@
+const responseStatus = require("./responseStatuses");
+
+function errorHandler(error) {
+  switch (error) {
+    case responseStatus.badRequest:
+      throw Error("Required fields cannot be blank.");
+    case responseStatus.forbiddenAccess:
+      throw Error("You are not authorized.");
+    case responseStatus.notFound:
+      throw Error("This resource does not exist.");
+    case responseStatus.conflict:
+      throw Error("User already exists.");
+    case responseStatus.serverError:
+      throw Error(
+        "The request could not be completed. Please try again."
+      );
+    default:
+      throw Error(error.message);
+  }
+}
+
+module.exports = errorHandler;
--- data/resolvers/index.js
+++ data/resolvers/index.js
@@ -1,4 +1,13 @@
+const candidateResolver = require('./candidateResolver')
+const judgeResolver = require('./judgeResolver')
+const officeResolver = require('./officeResolver')
+const proposalResolver = require('./proposalResolver')
+
 const rootResolver = {
+  ...candidateResolver,
+  ...judgeResolver,
+  ...officeResolver,
+  ...proposalResolver,
 };
 
 module.exports = { rootResolver };
